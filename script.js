2fecca0066fc24e3a8ca7c93c6ebf8aef311cd53
chip <114098392+dchipp@users.noreply.github.com>
2022-12-02 12:11:23 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 
 var r = document.querySelector(':root');
+var slider = document.getElementById("corner");
+var color = document.getElementById("color");
+
 var displayOne = {
     segments: Array.from(document.querySelectorAll("#displayOne .segment"))
 }
@@ -14,46 +17,62 @@ var displayFour = {
 }
 function updateDisplay(display, value){
     switch(value){
-        case 0: aA="white",bB="white",cC="white",dD="white",eE="white",fF="white",gG="transparent"; break;
-        case 1: aA="transparent",bB="white",cC="white",dD="transparent",eE="transparent",fF="transparent",gG="transparent"; break;
-        case 2: aA="white",bB="white",cC="transparent",dD="white",eE="white",fF="transparent",gG="white"; break;
-        case 3: aA="white",bB="white",cC="white",dD="white",eE="transparent",fF="transparent",gG="white"; break;
-        case 4: aA="transparent",bB="white",cC="white",dD="transparent",eE="transparent",fF="white",gG="white"; break;
-        case 5: aA="white",bB="transparent",cC="white",dD="white",eE="transparent",fF="white",gG="white"; break;
-        case 6: aA="white",bB="transparent",cC="white",dD="white",eE="white",fF="white",gG="white"; break;
-        case 7: aA="white",bB="white",cC="white",dD="transparent",eE="transparent",fF="transparent",gG="transparent"; break;
-        case 8: aA="white",bB="white",cC="white",dD="white",eE="white",fF="white",gG="white"; break;
-        case 9: aA="white",bB="white",cC="white",dD="white",eE="transparent",fF="white",gG="white"; break;
-        default: aA="white",bB="white",cC="white",dD="white",eE="white",fF="white",gG="transparent"; break;
+        case 0: aA=color.value,bB=color.value,cC=color.value,dD=color.value,eE=color.value,fF=color.value,gG="transparent"; break;
+        case 1: aA="transparent",bB=color.value,cC=color.value,dD="transparent",eE="transparent",fF="transparent",gG="transparent"; break;
+        case 2: aA=color.value,bB=color.value,cC="transparent",dD=color.value,eE=color.value,fF="transparent",gG=color.value; break;
+        case 3: aA=color.value,bB=color.value,cC=color.value,dD=color.value,eE="transparent",fF="transparent",gG=color.value; break;
+        case 4: aA="transparent",bB=color.value,cC=color.value,dD="transparent",eE="transparent",fF=color.value,gG=color.value; break;
+        case 5: aA=color.value,bB="transparent",cC=color.value,dD=color.value,eE="transparent",fF=color.value,gG=color.value; break;
+        case 6: aA=color.value,bB="transparent",cC=color.value,dD=color.value,eE=color.value,fF=color.value,gG=color.value; break;
+        case 7: aA=color.value,bB=color.value,cC=color.value,dD="transparent",eE="transparent",fF="transparent",gG="transparent"; break;
+        case 8: aA=color.value,bB=color.value,cC=color.value,dD=color.value,eE=color.value,fF=color.value,gG=color.value; break;
+        case 9: aA=color.value,bB=color.value,cC=color.value,dD=color.value,eE="transparent",fF=color.value,gG=color.value; break;
+        default: aA=color.value,bB=color.value,cC=color.value,dD=color.value,eE=color.value,fF=color.value,gG="transparent"; break;
     }
     display.segments[0].style.backgroundColor=aA;
     display.segments[1].style.backgroundColor=fF;
     display.segments[2].style.backgroundColor=bB;
     display.segments[3].style.backgroundColor=gG;
     display.segments[4].style.backgroundColor=eE;
-    display.segments[5].style.backgroundColor=cC;
+    display.segments[5].style.backgroundColor=cC
     display.segments[6].style.backgroundColor=dD;
 }
+if(window.innerWidth<600){
+    r.style.setProperty('--displayHeight', '100px');
+}
+
+
 var unit=0,decim=0;
+
 setInterval(()=>{
     
     var date = new Date();
     var hours = date.getHours();
     var minutes = date.getMinutes();
-    updateDisplay(displayOne, (hours.toString().length > 1)?parseInt(hours.toString()[0]):0);
-    updateDisplay(displayTwo, (hours.toString().length == 1)?parseInt(hours.toString()[0]):parseInt(hours.toString()[1]));
-    updateDisplay(displayThree, (minutes.toString().length > 1)?parseInt(minutes.toString()[0]):0);
-    updateDisplay(displayFour, (minutes.toString().length == 1)?parseInt(minutes.toString()[0]):parseInt(minutes.toString()[1]));
+    r.style.setProperty('--segmentColor', color.value);
+    updateDisplay(displayOne, (hours.toString().length > 1) ? parseInt(hours.toString()[0]) : 0);
+    updateDisplay(displayTwo, (hours.toString().length == 1) ? parseInt(hours.toString()[0]) : parseInt(hours.toString()[1]));
+    updateDisplay(displayThree, (minutes.toString().length > 1) ? parseInt(minutes.toString()[0]) : 0);
+    updateDisplay(displayFour, (minutes.toString().length == 1) ? parseInt(minutes.toString()[0]) : parseInt(minutes.toString()[1]));
 
-},1000)
+})
 function selectSize(element){
     var options = document.querySelectorAll(".options .option");
     options.forEach(x =>{
         x.classList.remove("active");
     })
     element.classList.add("active");
-    if(element.innerHTML=="Small")r.style.setProperty('--displayHeight', '100px');
-    if(element.innerHTML=="Medium")r.style.setProperty('--displayHeight', '260px');
-    if(element.innerHTML=="Large")r.style.setProperty('--displayHeight', '500px');
+    if(window.innerWidth>600){
+        if(element.innerHTML=="Small")r.style.setProperty('--displayHeight', '100px');
+        if(element.innerHTML=="Medium")r.style.setProperty('--displayHeight', '260px');
+        if(element.innerHTML=="Large")r.style.setProperty('--displayHeight', '500px');
+    } else {
+        if(element.innerHTML=="Small")r.style.setProperty('--displayHeight', '50px');
+        if(element.innerHTML=="Medium")r.style.setProperty('--displayHeight', '100px');
+        if(element.innerHTML=="Large")r.style.setProperty('--displayHeight', '140px');
+    }
     
 }
+corner.oninput = function() {
+    r.style.setProperty('--roundness', `${this.value/5}px`);
+  }
